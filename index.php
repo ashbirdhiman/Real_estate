a93059a717742d42ed0d51861a6be10d485190ee
ashbirdhiman <38061870+ashbirdhiman@users.noreply.github.com>
2023-12-22 18:39:21 -0500

    Creating about us page and index page

---
+++ index.php
@@ -0,0 +1,535 @@
+<html>
+<head><title>DownTownEstate </title>
+<meta charset="utf-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+	<style>
+	
+	.head{
+	background-color:#ffffff;
+	height:30px;
+	
+	
+	}
+	.foot1{
+	background-color:#ffffff;
+	height:30px;
+	
+	}
+	hr {
+            border: 1px solid red;
+        }
+		
+	.Real{
+	
+	font-size:40px;
+	color:white;
+	background-color:red;
+	
+	
+	}
+	
+	
+	
+	.header{
+	background-color:#ffffff;
+
+	
+	
+	}
+	.footer{
+		background-color:#ffffff;
+		
+	
+	}
+	.nav-link{
+	color:black;
+	
+	
+	
+	}
+	.nav-item{
+	font-family: 'Montserrat', sans-serif;
+	font-size: 20px;
+	font-weight: 400;
+	margin-top:20px;
+	}
+	.latest{
+	font-size:23px;
+	color:white;
+	background-color:#ffffff;
+	
+	}
+	.lprop{
+	font-size:20px;
+	color:white;
+	background-color:#ffffff;
+	
+	
+	
+	}
+	
+	
+	.prop1{
+	
+	height:100px;
+	background-color:none;
+	
+	
+	}
+	.prop2{
+	
+	font-size:50px;
+	color:#ffffff;
+	font-family: "Times New Roman", Times, serif;
+
+	}
+	.capt{
+	
+	font-size:20px;
+	color:gray;
+	letter-spacing:0px;
+	word-spacing:3px;
+	margin-top:-13px;
+
+	}
+.myButton {
+	
+	background-color:red;
+	-moz-border-radius:35px;
+	-webkit-border-radius:35px;
+	border-radius:35px;
+	border:2px solid red;
+	display:inline-block;
+	cursor:pointer;
+	color:#ffffff;
+	font-family:Arial;
+	font-size:15px;
+	font-weight:bold;
+	padding:12px 41px;
+	text-decoration:none;
+	text-shadow:0px 1px 0px #1570cd;
+}
+.myButton:hover {
+	background:-webkit-gradient(linear, left top, left bottom, color-stop(0.05, #1e62d0), color-stop(1, #3d94f6));
+	background:-moz-linear-gradient(top, #1e62d0 5%, #3d94f6 100%);
+	background:-webkit-linear-gradient(top, #1e62d0 5%, #3d94f6 100%);
+	background:-o-linear-gradient(top, #1e62d0 5%, #3d94f6 100%);
+	background:-ms-linear-gradient(top, #1e62d0 5%, #3d94f6 100%);
+	background:linear-gradient(to bottom, #1e62d0 5%, #3d94f6 100%);
+	filter:progid:DXImageTransform.Microsoft.gradient(startColorstr='#1e62d0', endColorstr='#3d94f6',GradientType=0);
+	background-color:#1e62d0;
+}
+.myButton:active {
+	position:relative;
+	top:1px;
+}
+.backg{
+height:320px;
+width:100%;
+background-image:url(./image/back.jpg);
+background-repeat:no-repeat;
+color:white;
+font-size:30px;
+font-family:"Times New Roman", Times, serif;
+background-size:cover;
+
+
+
+}
+		.bord
+		{
+		width:189px;
+		
+		font-size:23;
+		color:white;
+		border:4px solid;
+		border-color:#1c3e75;
+		border-radius: 15px 50px 30px;
+		text-align:center;
+		}
+		
+.space{
+
+height:100px;
+background:none;
+
+}
+
+.about{
+background:none;
+color:red;
+letter-spacing:20px;
+
+
+
+
+}
+
+.mob{
+margin-top:20px;
+font-size:20px;
+color:white;
+}
+.logo
+{
+margin-top:15px;
+
+}
+
+
+	
+	
+	
+	
+	
+	</style>
+</head>
+
+	<body>
+	<div class="container-fluid">	
+	<div class="head">
+					<div class="row">
+						<div class="col-sm-12"> 
+						</div>
+					</div>
+				</div>
+	<div class="header">
+								<div class="row">
+										<div class="col-sm-2">
+										</div>
+									
+									<div class="col-sm-1"> 
+										<div class="logo">
+											<a href="#">
+											<img src="image/logo.png" width="100px" >
+											</a>
+										</div>
+										
+									</div>
+									
+									
+									<div class="col-sm-">
+									<ul class="nav justify-content-center">
+											<li class="nav-item">
+												<a class="nav-link" href="http://localhost/finalweb/index.php">HOME</a>
+											  </li>
+											  <li class="nav-item">
+												<a class="nav-link" href="http://localhost/finalweb/properties.php">PROPERTIES</a>
+											  </li>
+											  <li class="nav-item">
+												<a class="nav-link" href="http://localhost/finalweb/contact.html">CONTACT</a>
+											  </li>
+											  
+											  <li class="nav-item">
+												<a class="nav-link" href="http://localhost/finalweb/enquire.php">ENQUIRE-US</a>
+											  </li>
+											  <li class="nav-item">
+												<a class="nav-link" href="http://localhost/finalweb/review.php">REVIEW-US</a>
+			
+												<li class="nav-item">
+													<a class="nav-link" href="http://localhost/finalweb/signin.php">SIGN IN</a>
+												  </li>
+											  </li>
+											  
+										</ul>
+
+								
+								
+								
+								
+								
+								</div>
+								
+						
+							</div>
+						</div>	  
+		  
+		  <div class="head">
+					<div class="row">
+						<div class="col-sm-12"> 
+						</div>
+					</div>
+			</div>
+			
+			<hr>
+			<div class="row">
+		  
+				<div class="col-sm-12">
+					<div id="carouselExampleSlidesOnly" class="carousel slide" data-ride="carousel">
+						  <div class="carousel-inner">
+							<div class="carousel-item active">
+								<img src="image/slide1.jpg" class="d-block w-100" alt="..." style="height:650px;">
+							</div>
+							<div class="carousel-item">
+							  <img src="image/slide2.jpg" class="d-block w-100" alt="..." style="height:650px;">
+							</div>
+							<div class="carousel-item">
+							  <img src="image/slide3.jpg" class="d-block w-100" alt="..." style="height:650px;">
+							</div>
+						  </div>
+					</div>
+				</div>
+				
+		  </div>
+		  
+		  <div class="prop1">
+					<div class="row">
+				  
+						<div class="col-sm-12">
+					  
+					  
+							
+					  
+						</div>
+				   </div>
+				  
+		  </div>
+		  
+		  <div class="prop2">
+				<div class="row">
+						<div class="col-sm-1">
+			
+						</div>
+						
+						<div class="col-sm-11">
+							RECENT PROPERTIES<br>
+			
+						
+							<div class="capt">
+							<center><img src="image/logo.png" width="100px" >
+								<p >Search your dream home</p><center>
+							</div>	
+						
+						</div>
+					  
+				</div>
+				
+			</div>	
+			<div class="row">
+				  
+						<div class="col-sm-12" style="background-color:none; height:35px;">
+					  
+						</div>
+			</div>
+			
+			<div class="row">
+		  
+				<div class="col-sm-1">
+		  
+				</div>
+				
+				
+				<div class="col-sm-10">
+					<div class="card-container">
+						<?php
+						// Connect to your database (replace with your actual database credentials)
+						include("connect.php"); 
+
+				
+						// Retrieve property data from the database
+						$sql = "SELECT DISTINCT properties.*, agent.email,agent.username FROM properties JOIN agent_property ON properties.id = agent_property.property_id JOIN agent ON agent_property.agent_id = agent.agent_id where id<3;";
+						$result = $conn->query($sql);
+				
+						if ($result->num_rows > 0) {
+							while ($row = $result->fetch_assoc()) {
+								echo '<div class="card">';
+								echo '<div class="card-header">Property</div>';
+								echo '<div class="card-body">';
+								echo '<table>';
+								echo '<tr><th>Location</th><td>' . $row["location"] . '</td></tr>';
+								echo '<tr><th>Rooms</th><td>' . $row["rooms"] . '</td></tr>';
+								echo '<tr><th>Price</th><td>$' . number_format($row["price"], 2) . '</td></tr>';
+								echo '<tr><th>Contact</th><td>' . $row["email"] . '</td></tr>';
+								echo '</table>';
+								echo '</div>';
+								echo '</div>';
+							}
+						} else {
+							echo "<p>No properties found in the database.</p>";
+						}
+				
+						// Close the database connection
+						$conn->close();
+						?>
+					</div>
+				</div>
+				
+		  
+				</div>
+				
+				<div class="col-sm-1">
+		  
+				</div>
+		  </div>
+		  
+		  <div class="row">
+		  
+				<div class="col-sm-12" style="background-color:none; height:40px;">
+		  
+				</div>
+		  </div>
+	
+			<div class="row">	  
+					<div class="col-sm-5">
+					</div>
+					<div class="col-sm-7">
+						<a href="http://localhost/finalweb/properties.php" class="myButton" style="text-decoration:none;">SEE MORE</a>
+					</div>
+			 </div>
+			 
+			 
+			<div class="row">
+		  
+				<div class="col-sm-12" style="background-color:none; height:40px;">
+		  
+				</div>
+		  </div>
+		
+			<div class="row">
+					 <div class="backg">
+						
+						<div class="space">
+						
+						</div>
+						<div class="col-sm-3">
+							
+							
+						</div>
+						
+						<div class="col-sm-9">
+						<b>ARE YOU BUYING OR SELLING?</b>
+							
+							
+						</div>
+						
+				  </div>
+				  
+		  
+		  </div>
+		  
+		  <div class="row">
+				  
+										
+										<div class="col-sm-1">
+										
+										</div>
+										<div class="col-sm-10">
+											 <br>	  
+											<h1 class="about" align="center" style="font-family:"Times New Roman;", Times, serif; "><b>ABOUT-US</b></h1>
+											 <br>	  
+											<p style="text-align:center;font-size:20px;font-family:Arial, Helvetica, sans-serif;
+											"><i>As the nation’s No. 1 independent brand (by sales volume), DownTownEstate has guided people home since 2019. We’re a full-service brokerage firm, offering residential and commercial real estate, mortgage, settlement, insurance, property management, corporate relocation and vacation rental services.</i>
+											</p>
+											
+											<p style="text-align:center;font-size:20px;font-family:Arial, Helvetica, sans-serif;">
+											<i>A focus on exceptional service to clients and support for our real estate agents means being tech-savvy and digitally connected. Our part ownership in Moxi Works, a real estate technology company, keeps our agents on the cutting edge.</i>
+											</p>
+											
+											<p style="text-align:center;font-size:20px;font-family:Arial, Helvetica, sans-serif;">
+											<i>But that’s just our business; our success is defined by being so much more. We are a network of thousands of agents and industry professionals united in a passion for real estate. We are facilitators connecting people with places to call home. And most importantly, in the communities where we live and work, we are trusted insiders, friends and neighbors</i>
+											</p>
+										</div>
+										<div class="col-sm-1">
+										</div>
+						 
+				  
+			</div>
+			
+			  <br>
+			  
+				<hr>
+			
+					<div class="foot1">
+						<div class="row">
+							<div class="col-sm-12"> 
+							</div>
+						</div>
+					</div>
+				
+				
+			
+				
+			<div class="footer">
+						<div class="row">
+							<div class="col-sm-2">
+							</div>
+						
+						<div class="col-sm-1"> 
+							<div class="logo">
+								<a href="#">
+								<img src="image/llogo.png"  >
+								</a>
+							</div>
+							
+						</div>
+						
+						
+					<div class="col-sm-9">
+					
+					<ul class="nav justify-content-center">
+											<li class="nav-item">
+												<a class="nav-link" href="http://localhost/finalweb/index.php">HOME</a>
+											  </li>
+											  <li class="nav-item">
+												<a class="nav-link" href="http://localhost/finalweb/properties.php">PROPERTIES</a>
+											  </li>
+											  <li class="nav-item">
+												<a class="nav-link" href="http://localhost/finalweb/contact.html">CONTACT</a>
+											  </li>
+											  
+											  <li class="nav-item">
+												<a class="nav-link" href="http://localhost/finalweb/enquire.php">ENQUIRE-US</a>
+											  </li>
+											  <li class="nav-item">
+												<a class="nav-link" href="http://localhost/finalweb/review.php">REVIEW-US</a>
+			
+												<li class="nav-item">
+													<a class="nav-link" href="http://localhost/finalweb/signin.php">SIGN IN</a>
+												  </li>
+											  </li>
+											  
+										</ul>
+
+					</div>
+					
+					
+			
+				</div>
+			</div>	  
+		  
+			
+			
+			
+			</div>
+			<div class="Real">
+				<div class ="row">
+					<div class="col-sm-3">
+					</div>
+					<div class="col-sm-6">
+						<br>
+						<p style="text-align:center;"><b>DownTownEstate THE REAL ONE !</p>
+					</div>
+				
+					<div class="col-sm-3">
+						
+					
+					</div>
+				
+				
+			
+				</div>
+			</div>
+		
+	</div>
+
+	
+	
+		<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+		<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+	</body>
+
+</html>
