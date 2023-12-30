f33334b5b16a6e19c0149bb89e6e84d9549871dc
ashbirdhiman <38061870+ashbirdhiman@users.noreply.github.com>
2023-12-30 14:23:43 -0500

    Adding logo and enquire page with form

---
+++ enquire.php
@@ -0,0 +1,423 @@
+
+
+<html>
+<head><title>contact </title>
+<meta charset="utf-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+		<style>
+		
+		.head{
+	background-color:#ffffff;
+	height:30px;
+	
+	
+	}
+	.header{
+	background-color:#ffffff;
+
+	
+	
+	}
+	hr {
+            border: 1px solid red;
+        }
+	.mob{
+margin-top:20px;
+font-size:20px;
+color:white;
+}
+.nav-link{
+	color:red;
+	
+	
+	
+	}
+	.nav-item{
+	font-family: 'Montserrat', sans-serif;
+	font-size: 20px;
+	font-weight: 400;
+	margin-top:20px;
+	}
+	.logo
+{
+margin-top:15px;
+
+}
+.bord
+		{
+		width:189px;
+		
+		font-size:23;
+		color:white;
+		border:4px solid;
+		border-color:#1c3e75;
+		border-radius: 15px 50px 30px;
+		text-align:center;
+		}
+		
+.parallax_background{
+height:320px;
+width:100%;
+background-image:url(./image/back.jpg);
+background-repeat:no-repeat;
+color:white;
+font-size:30px;
+font-family:"Times New Roman", Times, serif;
+background-size:cover;
+
+
+
+}
+.space{
+
+height:100px;
+background:none;
+
+}		
+.realtors {
+    width: 100%;
+    background: #f0f2f7;
+    padding-top: 105px;
+    padding-bottom: 74px;
+	}
+	
+.foot1{
+	background-color:#ffffff;
+	height:30px;
+	
+	}
+.footer{
+		background-color:#ffffff;
+		
+	
+	}
+.logo
+{
+margin-top:15px;
+
+}
+.latest{
+	font-size:23px;
+	color:white;
+	background-color:#ffffff;
+	
+	}
+	.lprop{
+	font-size:20px;
+	color:white;
+	background-color:#ffffff;
+	
+	
+	
+	}
+		
+.Real{
+	
+	font-size:40px;
+	color:white;
+	background-color:#ffffff;
+	
+	
+	}
+	
+		
+		
+		
+    .container {
+            width: 50%;
+            margin: auto;
+            overflow: hidden;
+            padding: 10px;
+            background-color: #fff;
+            margin-top: 50px;
+            border-radius: 10px;
+            box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+        }
+
+        label {
+            display: block;
+            margin: 10px 0 5px;
+        }
+
+        input, select, textarea {
+            width: 100%;
+            padding: 8px;
+            margin: 5px 0 15px;
+            display: inline-block;
+            border: 1px solid #ccc;
+            box-sizing: border-box;
+            border-radius: 4px;
+        }
+
+        button {
+            background-color: red;
+            color: white;
+            padding: 10px 15px;
+            margin: 8px 0;
+            border: none;
+            border-radius: 4px;
+            cursor: pointer;
+            font-size: 16px;
+        }
+
+        button:hover {
+            background-color: #45a049;
+        }
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	
+		</style>
+</head>
+
+	<body>
+	
+	
+	
+	
+	<div class="container-fluid">
+			
+				
+					
+		<div class="head">
+			<div class="row">
+				<div class="col-sm-12"> 
+				</div>
+			</div>
+		</div>		
+				
+		
+						<div class="header">
+								<div class="row">
+										<div class="col-sm-2">
+										</div>
+									
+									<div class="col-sm-1"> 
+										<div class="logo">
+											<a href="#">
+											<img src="image/logo.png" width="100px" >
+											</a>
+										</div>
+										
+									</div>
+									
+									
+									<div class="col-sm-">
+								
+                                    <ul class="nav justify-content-center">
+											<li class="nav-item">
+												<a class="nav-link" href="http://localhost/finalweb/index.php">HOME</a>
+											  </li>
+											  <li class="nav-item">
+												<a class="nav-link" href="http://localhost/finalweb/properties.php">PROPERTIES</a>
+											  </li>
+											  <li class="nav-item">
+												<a class="nav-link" href="http://localhost/finalweb/contact.html">CONTACT</a>
+											  </li>
+											  
+											  <li class="nav-item">
+												<a class="nav-link" href="http://localhost/finalweb/enquire.php">ENQUIRE-US</a>
+											  </li>
+											  <li class="nav-item">
+												<a class="nav-link" href="http://localhost/finalweb/review.php">REVIEW-US</a>
+			
+												<li class="nav-item">
+													<a class="nav-link" href="http://localhost/finalweb/signin.php">SIGN IN</a>
+												  </li>
+											  </li>
+											  
+										</ul>
+
+
+								
+								</div>
+								
+						
+							</div>
+						</div>	  
+		  
+		  <div class="head">
+					<div class="row">
+						<div class="col-sm-12"> 
+						</div>
+					</div>
+			</div>	  
+		  
+		  <div class="head">
+					<div class="row">
+						<div class="col-sm-12"> 
+						</div>
+					</div>
+			</div>
+			<hr>
+			<div class="container-fluid">
+						<div class="row">
+								 <div class="parallax_background parallax-window" data-parallax="scroll"  data-speed="0.8">
+									
+									
+									<div class="col-sm-2">
+										
+										
+									</div>
+									
+									<div class="col-sm-10"style="margin-left:220px;">
+									
+									
+										
+										
+									</div>
+									
+								</div>
+							  
+					  
+					  </div>
+					  
+			</div>
+		
+		
+		
+			
+				
+			<div class="container-fluid">
+                <div class="nav-item">
+                    <center>Please send the enquiry using below form</center>
+                </div>
+                <br>
+                <br>
+				<form  method="post">
+                    <label for="user_email">User Email:</label>
+                    <input type="email" id="user_email" name="user_email" required>
+            
+                    <label for="property">Select Property:</label>
+                    <select id="property" name="property" required>
+                        <option value="property1">Property 1</option>
+                        <option value="property2">Property 2</option>
+                        <option value="property3">Property 3</option>
+                        <!-- Add more options as needed -->
+                    </select>
+            
+                    <label for="date">Date:</label>
+                    <input type="date" id="date" name="date" required>
+            
+                    <label for="description">Description:</label>
+                    <textarea id="description" name="description" rows="4" required></textarea>
+            
+                   <center> <button type="submit">Submit</button></center>
+                </form>
+			</div>
+			
+			<?php
+                if ($_SERVER["REQUEST_METHOD"] == "POST") {
+                    // Retrieve form data
+                    $user_email = $_POST["user_email"];
+                    $property = $_POST["property"];
+                    $date = $_POST["date"];
+                    $description = $_POST["description"];
+
+                    include("connect.php"); 
+
+                    // Map property names to property_id values (replace these with your actual property IDs)
+                    $propertyMapping = [
+                        "property1" => 1,
+                        "property2" => 2,
+                        "property3" => 3,
+                    ];
+
+                    // Check if the selected property exists in the mapping
+                    if (array_key_exists($property, $propertyMapping)) {
+                        $property_id = $propertyMapping[$property];
+
+                        // Insert data into the 'enquiries' table
+                        $sql = "INSERT INTO enquiries (user_email, property_id, enquiry_date, enquiry_description)
+                                VALUES ('$user_email', $property_id, '$date', '$description')";
+
+                        if ($conn->query($sql) === TRUE) {
+                        
+                        } else {
+                        
+                        }
+                    } else {
+                        echo "Invalid property selection";
+                    }
+
+                    // Close the database connection
+                    $conn->close();
+                }
+?>	
+		  
+		  <br>
+		  <hr>
+					<div class="foot1">
+						<div class="row">
+							<div class="col-sm-12"> 
+							</div>
+						</div>
+					</div>
+					
+			<div class="footer">
+						<div class="row">
+							<div class="col-sm-1">
+							</div>
+						
+						<div class="col-sm-2"> 
+							<div class="logo">
+								<a href="#">
+								<img src="image/llogo.png"  >
+								</a>
+							</div>
+							
+						</div>
+						
+						
+					<div class="col-sm-7">
+					
+					<ul class="nav justify-content-center">
+											<li class="nav-item">
+												<a class="nav-link" href="http://localhost/finalweb/index.php">HOME</a>
+											  </li>
+											  <li class="nav-item">
+												<a class="nav-link" href="http://localhost/finalweb/properties.php">PROPERTIES</a>
+											  </li>
+											  <li class="nav-item">
+												<a class="nav-link" href="http://localhost/finalweb/contact.html">CONTACT</a>
+											  </li>
+											  
+											  <li class="nav-item">
+												<a class="nav-link" href="http://localhost/finalweb/enquire.php">ENQUIRE-US</a>
+											  </li>
+											  <li class="nav-item">
+												<a class="nav-link" href="http://localhost/finalweb/review.php">REVIEW-US</a>
+			
+												<li class="nav-item">
+													<a class="nav-link" href="http://localhost/finalweb/signin.php">SIGN IN</a>
+												  </li>
+											  </li>
+											  
+										</ul>
+
+					
+					</div>
+			
+				</div>
+				
+			</div>
+			
+				
+	</div>
+		<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+		<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+	</body>
+
+</html>
